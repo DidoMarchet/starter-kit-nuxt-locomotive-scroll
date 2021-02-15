de86469f86887b0403746a6c8d6e6191091e0bad
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-02-15 16:43:37 +0100

    New app boilerplate

--- configuration/sitemap.js
+++ configuration/sitemap.js
@@ -1,10 +1,2 @@
 // https://sitemap.nuxtjs.org/
-export default {
-  sitemap: {
-    hostname: process.env.BASE_URL,
-    gzip: true,
-    routes: () => {
-      return []
-    },
-  },
-}
+export default {}
