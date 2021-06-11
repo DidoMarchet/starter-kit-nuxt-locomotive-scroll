765ec4f69979ea25fdb9aa262ca805173e8aec68
Davide Marchet <38940340+DidoMarchet@users.noreply.github.com>
2021-06-11 15:24:23 +0200

    Update env for sitemap.xml

--- configuration/sitemap.js
+++ configuration/sitemap.js
@@ -1,4 +1,11 @@
 // https://sitemap.nuxtjs.org/
 export default {
-  sitemap: {},
+  sitemap: {
+    hostname: process.env.NUXT_ENV_BASE_URL,
+    gzip: true,
+    routes: () => {
+      return []
+    },
+  },
 }
+
