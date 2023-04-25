5b9c4228c491234c9a836b3b10b943fe81238f1b
Davide <io@davidemarchet.com>
2023-04-25 10:36:57 +0200

    Uopdates modules and libraries

--- configuration/modules.js
+++ configuration/modules.js
@@ -1,13 +1,13 @@
 // https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-modules
 export default {
   modules: [
-    'nuxt-i18n',
     '@nuxtjs/style-resources',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     '@nuxtjs/apollo',
     '@nuxtjs/sitemap',
     '@nuxtjs/gtm',
+    '@nuxtjs/i18n',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
